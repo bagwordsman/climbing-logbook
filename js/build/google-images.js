c6fff6db969f1a26a9305c98d5b6934a42f8c477
Martin Bagshaw <martinbagshaw1@gmail.com>
2018-06-29 17:21:30 +0100

    add favourite feature - first draft

--- js/build/google-images.js
+++ js/build/google-images.js
@@ -1,10 +1,18 @@
 'use strict';
+// this thing is still a fucker, see notes file
+// fs.readFileSync throws an error
 
 
 // const imageSearch = require('node_modules/node-google-image-search-promise');
 
 
+// require('dotenv').config();
+// require('dotenv').config({path: '.env'})
+
+
 const imageSearch = require('../../node_modules/node-google-image-search-promise');
+
+// console.log(imageSearch());
 
 imageSearch('Cloggy', 0, 5)
   .then(results => {
